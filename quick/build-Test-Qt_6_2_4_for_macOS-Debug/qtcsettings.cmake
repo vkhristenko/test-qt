978b449982cf4291deb1fde83b8d31c8d85665ef
Viktor Khristenko <vkhristenko@cmtam.com>
2022-06-09 17:04:03 +0200

    rm build products

--- quick/build-Test-Qt_6_2_4_for_macOS-Debug/qtcsettings.cmake
+++
@@ -1,9 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/vkhristenko/soft/qt-sdk/Qt Creator.app/Contents/Resources/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "/Users/vkhristenko/soft/qt-sdk/6.2.4/macos/bin/qmake" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "/Users/vkhristenko/soft/qt-sdk/6.2.4/macos" CACHE "PATH" "" FORCE)
-set("CMAKE_C_COMPILER" "/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
